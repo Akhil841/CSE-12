9619144c6f99ff1d429e940712a61e410816dbaf
Akhil841 <44041958+Akhil841@users.noreply.github.com>
2022-02-01 14:43:15 -0800

    Added custom unit tests, commented code, added Iterator housekeeping methods

--- cse12-wi22-pa4-Iterator-starter/MyLinkedList.java
+++ cse12-wi22-pa4-Iterator-starter/MyLinkedList.java
@@ -3,13 +3,12 @@
  * Email: avpillai@ucsd.edu
  * Sources used: None
  * 
- * 2-4 sentence file description here
- * 
  * This file contains my implementation of a LinkedList, as well as the provided Node class.
- * This file also contains my implementation of an Iterator.
+ * This file also contains my implementation of an Iterator for the LinkedList.
  */
 
 import java.util.AbstractList;
+import java.util.Iterator;
 import java.util.ListIterator;
 import java.util.NoSuchElementException;
 /** 
@@ -96,8 +95,9 @@ public class MyLinkedList<E> extends AbstractList<E> {
 	}
 
 	//  Implementation of the MyLinkedList Class
-	/** Only 0-argument constructor is defined */
-	//initializes empty LL with sentinel nodes
+	/**
+	* Initializes empty LinkedList with head and tail nodes.
+	*/
 	public MyLinkedList() {
 		//initialize head and tail
 		head = new Node(null);
@@ -110,21 +110,28 @@ public class MyLinkedList<E> extends AbstractList<E> {
 		tail.setNext(null);
 		/* Add your implementation here */
 	}
-	//Returns the size of the LL
+	/**
+	 * Returns the number of elements in the LinkedList.
+	 */
 	@Override
 	public int size() {
 		// return size variable that is updated by the methods
 		return size;
 	}
-
+	/**
+	 * Returns the element at a specific index.
+	 */
 	@Override
 	public E get(int index) {
 		//use getNth helper method to get Node at the given index
 		//return the element at that specific Node
 		return getNth(index).getElement();
 	}
-	//Adds the provided data to the LL in a new node,
-	//adjusting the pointers accordingly.
+	/**
+	 * Adds the provided data to a LinkedList
+	 * at the provided index, adjusting the pointers of nearby
+	 * nodes accordingly.
+	 */
 	@Override
 	public void add(int index, E data) {
 		//Only non-null data can be added
@@ -150,15 +157,19 @@ public class MyLinkedList<E> extends AbstractList<E> {
 		size++;
 	}
 	
-	//Appends a new element to the end of the LL
+	/**
+	 * Appends a new element to the end of the LinkedList.
+	 */
 	public boolean add(E data) {
 		//Use the above add(int, E), with the index parameter being the end of the list
 		add(size, data);
 		return true;
 	}
 
-	//Sets the provided data into the node at the provided index, 
-	//and returns the overwritten data.
+	/**
+	 * Sets the provided data into the node at the provided index, 
+	 * and returns the overwritten data.
+	 */
 	public E set(int index, E data) {
 		//Only non-null data may be inserted.
 		if (data == null) throw new NullPointerException();
@@ -171,8 +182,10 @@ public class MyLinkedList<E> extends AbstractList<E> {
 		//Return the overwritten data.
 		return output;
 	}
-	//Removes the node at the provided index from the LL,
-	//and returns its data.
+	/**
+	 * Removes the node at the provided index from the LinkedList,
+	 * and returns its data.
+	 */
 	public E remove(int index) {
 		//Get the element to return
 		E output = getNth(index).getElement();
@@ -195,7 +208,9 @@ public class MyLinkedList<E> extends AbstractList<E> {
 		//Return the removed data.
 		return output;
 	}
-	//Removes all data from the LL.
+	/**
+	 * Removes all data from the LL.
+	 */
 	public void clear() {
 		//Point the head and tail at each other, so all data 
 		//in the list is effectively removed.
@@ -204,12 +219,18 @@ public class MyLinkedList<E> extends AbstractList<E> {
 		//Set size to 0 since list is now empty.
 		size = 0;
 	}
-	//Returns true if the LL has no elements, false otherwise.
+	/**
+	 * Returns true if the LL has no elements, false otherwise.
+	 */
 	public boolean isEmpty() {
 		//Return true if the size is 0, false otherwise.
 		return size == 0;
 	}
-	//Gets the node at the provided index.
+	/**
+	 * Gets the node at the provided index.
+	 * @param index The index of the LinkedList to
+	 * get the node from.
+	 */
 	protected Node getNth(int index) {
 		//Only existing elements may be accessed.
 		if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
@@ -223,12 +244,40 @@ public class MyLinkedList<E> extends AbstractList<E> {
 		//Return the node found at the provided index.
 		return curNode;
 	}
+	/**
+	 * Implementation of a ListIterator for a MyLinkedList.
+	 * This is a protected class, so it can only be declared
+	 * via an instance of a MyLinkedList.
+	 */
 	protected class MyListIterator implements ListIterator<E> {
+		/**
+		 * The node on the MyListIterator's left.
+		 */
 		Node left;
+		/**
+		 * The node on the MyListIterator's right
+		 */
 		Node right;
+		/**
+		 * The index of the MyLinkedList that the
+		 * MyListIterator is positioned at.
+		 */
 		int idx;
+		/**
+		 * True if the MyLinkedList is being traversed
+		 * head to tail, false otherwise.
+		 */
 		boolean forward;
+		/**
+		 * True if the last modified element
+		 * can be removed or set, false otherwise.
+		 */
 		boolean canRemoveOrSet;
+		/**
+		 * 0-arg constructor for the Iterator.
+		 * Initializes all of the instance variables with data
+		 * from the MyLinkedList used to instantiate it.
+		 */
 		public MyListIterator() {
 			left = head;
 			right = head.getNext();
@@ -236,59 +285,116 @@ public class MyLinkedList<E> extends AbstractList<E> {
 			forward = true;
 			canRemoveOrSet = false;
 		}
-
+		/**
+		 * @return Whether the Iterator can traverse forward by 1.
+		 */
 		public boolean hasNext() {
+			//if right == tail, the list is at its end
+			//and we cannot traverse further.
 			return right != tail;
 		}
 
+		/**
+		 * Moves the iterator forward by 1 index, and shifts the 
+		 * instance variables accordingly.
+		 * @return The next element in the list.
+		 */
 		public E next() {
+			//Only traverse forward if there are elements
+			//to traverse to
 			if (!hasNext()) throw new NoSuchElementException();
+			//Shift all the instance variables to the right
 			left = right;
 			right = right.getNext();
 			idx++;
+			//Update traversion to forward
 			forward = true;
 			canRemoveOrSet = true;
 			return left.getElement();
 		}
-
+		/**
+		 * @return Whether the Iterator can traverse backwards by 1.
+		 */
 		public boolean hasPrevious() {
+			//if left == head, the list is at its beginning
+			//and we cannot traverse forward.
 			return left != head;
 		}
-
+		/**
+		 * Moves the iterator backward by 1 index, and shifts the 
+		 * instance variables accordingly.
+		 * @return The previous element in the list.
+		 */
 		public E previous() {
+			//Only traverse backward if there are elements
+			//to traverse to
 			if (!hasPrevious()) throw new NoSuchElementException();
+			//Shift all the instance variables to the left
 			right = left;
 			left = left.getPrev();
 			idx--;
+			//Update traversion to backward
 			forward = false;
 			canRemoveOrSet = true;
 			return right.getElement();
 		}
 
+		/**
+		 * @return The index of the object returned by {@link #next} if it exists,
+		 * list size otherwise.
+		 */
 		public int nextIndex() {
 			return idx;
 		}
 
+		/**
+		 * @return The index of the object returned  by {@link #previous} if it exists,
+		 * -1 otherwise.
+		 */
 		public int previousIndex() {
 			return (!hasPrevious()) ? -1 : idx - 1;
 		}
 
+		/**
+		 * Adds an element to the index immediately before the element returned by {@link #previous}.
+		 * @param element The element to be inserted into the {@link MyLinkedList}
+		 * @throws NullPointerException if you attempt to insert a null element
+		 */
 		public void add(E element) {
+			//Only non-null elements may be added.
 			if (element == null) throw new NullPointerException();
+			//Wrap element in a node and adjust the pointers to 
+			//integrate the node into the LL
 			Node newNode = new Node(element);
+			//Update the involved pointers
 			left.setNext(newNode);
 			right.setPrev(newNode);
 			newNode.setPrev(left);
 			newNode.setNext(right);
+			//Update the iterator's instance variables
 			left = newNode;
 			idx++;
 			canRemoveOrSet = false;
+			//Update LL size
+			size++;
 		}
 
+		/**
+		 * Sets the latest element called by {@link #next} or {@link #previous}
+		 * to a new {@link Node} with value element.
+		 * @param element The value that will replace the original value
+		 * of the selected {@link Node}.
+		 * @throws IllegalStateException if a new value cannot currently be removed or set
+		 * @throws NullPointerException if user attempts to set null
+		 */
 		public void set(E element) {
+			//Throw exceptions if necessary
 			if (!canRemoveOrSet) throw new IllegalStateException();
 			if (element == null) throw new NullPointerException();
+			//Wrap new value in node
 			Node setThis = new Node(element);
+			//Update list and pointers depending on
+			//array traversal direction
 			if (forward) {//set left
 				Node previous = left.getPrev();
 				left = setThis;
@@ -303,8 +409,15 @@ public class MyLinkedList<E> extends AbstractList<E> {
 			}
 		}
 
+		/**
+		 * Removes the latest element called by {@link #next} or {@link #previous}.
+		 * @throws IllegalStateException if a new value cannot currently be removed or set
+		 */
 		public void remove() {
+			//Throw exception if necessary
 			if (!canRemoveOrSet) throw new IllegalStateException();
+			//Update list and pointers depending on array traversal
+			//direction
 			if (forward) {//remove left
 				left = left.getPrev();
 				left.setNext(right);
@@ -315,8 +428,25 @@ public class MyLinkedList<E> extends AbstractList<E> {
 				left.setNext(right);
 				right.setPrev(left);
 			}
+			//Update instance variables if necessary.
 			if (forward) idx--;
 			canRemoveOrSet = false;
+			//Update LL size
+			size--;
+		}
+
+		/**
+		 * @return A new instance of a MyListIterator.
+		 */
+		public ListIterator<E> listIterator() {
+			return new MyListIterator();
+		}
+
+		/**
+		 * @return A new instance of a MyListIterator.
+		 */
+		public Iterator<E> iterator() {
+			return new MyListIterator();
 		}
 	}
 }
--- cse12-wi22-pa4-Iterator-starter/MyLinkedListCustomTester.java
+++ cse12-wi22-pa4-Iterator-starter/MyLinkedListCustomTester.java
@@ -1,114 +1,160 @@
 
 /**
- * TODO: Add your file header
- * Name:
- * ID:
- * Email:
- * Sources used: Put "None" if you did not have any external help
- * Some example of sources used would be Tutors, Zybooks, and Lecture Slides
- * 2-4 sentence file description here
+ * Name: Akhil Pillai
+ * ID: A16724533
+ * Email: avpillai@ucsd.edu
+ * Sources used: None
+ * 
+ * This file contains tests that I wrote to test the efficacy of my 
+ * implementation of a ListIterator. It tests cases that are different
+ * from those of the public tester.
  */
 
 import static org.junit.Assert.*;
+
+import java.util.NoSuchElementException;
+
 import org.junit.*;
 
 /**
- * TODO: Add your class header
- * 
- * IMPORTANT: Do not change the method headers and points are awarded
- * only if your test cases cover cases that the public tester file
- * does not take into account.
+ * This class all the tests I have written for MyListIterator's various methods.
+ * It also contains a setUp method to streamline initialization
+ * of MyLinkedLists for testing.
  */
 public class MyLinkedListCustomTester {
+    private MyLinkedList<String> listLen0, listLen1, listLen2;
+    private MyLinkedList<String>.MyListIterator listLen0Iter, listLen1Iter, listLen2Iter;
 
     /**
-     * This sets up the test fixture. JUnit invokes this method before
-     * every testXXX method. The @Before tag tells JUnit to run this method
-     * before each test.
+     * Initializes up the LinkedLists we will use before execution.
+     * These are the same LinkedLists as are used in the public tester.s
      */
     @Before
     public void setUp() throws Exception {
+        listLen0 = new MyLinkedList<>();
+        listLen0Iter = listLen0.new MyListIterator();
 
+        listLen1 = new MyLinkedList<>();
+        listLen1.add("Christine");
+        listLen1Iter = listLen1.new MyListIterator();
+
+        listLen2 = new MyLinkedList<>();
+        listLen2.add("Paul");
+        listLen2.add("Cao");
+        listLen2Iter = listLen2.new MyListIterator();
     }
 
     /**
-     * TODO: test the hasNext method when [fill in a possible edge case here]
+     * Test the hasNext method on an empty list
      */
     @Test
     public void testHasNext() {
-
+        //This should return false since listLen0Iter.right = listLen0.tail
+        //as per the constructor
+        assertFalse("hasNext() should return false for an empty list", listLen0Iter.hasNext());
     }
 
     /**
-     * TODO: test the next method when [...]
+     * Test the next method on an empty list
      */
-    @Test
+    @Test(expected = NoSuchElementException.class)
     public void testNext() {
-
+        //This should throw a NoSuchElementException since there is no element 
+        //in the list to return
+        String out = listLen0Iter.next();
     }
 
     /**
-     * TODO: test the hasPrevious method when [fill in another one here]
+     * Test the hasPrevious method on an empty list
      */
     @Test
     public void testHasPrevious() {
-
+        //This should return false since listLen0Iter.left = listLen0.head
+        //as per the constructor
+        assertFalse("hasNext() should return false for an empty list", listLen0Iter.hasPrevious());
     }
 
     /**
-     * TODO: test the previous method when [...]
+     * Test the previous method on an empty list
      */
-    @Test
+    @Test(expected = NoSuchElementException.class)
     public void testPrevious() {
-
+        //This should throw a NoSuchElementException since there is no element 
+        //in the list to return
+        String out = listLen0Iter.previous();
     }
 
     /**
-     * TODO: test the nextIndex method when [...]
+     * Test the nextIndex method on the middle of a list
      */
     @Test
     public void testNextIndex() {
-
+        //Add another element to the list to test in the middle
+        listLen2.add("CSE12");
+        //Iterate to the next element in the list
+        //(We know that the next method works from the previous)
+        //tests so we use it here
+        listLen2Iter.next();
+        listLen2Iter.next();
+        //nextIndex should be 2 at this point since that 
+        //is the index of the final value in the 
+        //LinkedList.
+        assertEquals(2, listLen2Iter.nextIndex());
     }
 
     /**
-     * TODO: test the previousIndex method when [...]
+     * Test the previousIndex method on an empty list
      */
     @Test
     public void testPreviousIndex() {
-
+        //Since there is no element to go backwards to,
+        //this should return -1.
+        assertEquals(-1, listLen0Iter.previousIndex());
     }
 
     /**
-     * TODO: test the set method when [...]
+     * Test the set method before using next or previous
      */
-    @Test
+    @Test(expected = IllegalStateException.class)
     public void testSet() {
-
+        //This should return an IllegalStateException since
+        //the Iterator doesn't know which value to set to
+        listLen1Iter.set("Professor");
     }
 
     /**
-     * TODO: test the remove method when [...]
+     * Test the remove method before using next or previous
      */
-    @Test
+    @Test(expected = IllegalStateException.class)
     public void testRemoveTestOne() {
-
+        //This should return an IllegalStateException since
+        //the Iterator doesn't know which value to remove
+        listLen1Iter.remove();
     }
 
     /**
-     * TODO: test the remove method when [fill in another one here]
+     * Test the remove method on a list with only one item
      */
     @Test
     public void testRemoveTestTwo() {
-
+        //Use next() to update canRemoveOrSet
+        listLen1Iter.next();
+        //canRemoveOrSet should be true
+        assertEquals(true, listLen1Iter.canRemoveOrSet);
+        //Use remove();
+        listLen1Iter.remove();
+        //List should be empty
+        assertEquals(listLen1.size, 0);
+        //canRemoveOrSet should be false
+        assertEquals(false, listLen1Iter.canRemoveOrSet);
     }
 
     /**
-     * TODO: test the add method when [...]
+     * Attempt to add a null value
      */
-    @Test
+    @Test(expected = NullPointerException.class)
     public void testAdd() {
-
+        listLen0Iter.add(null);
     }
 
 }
